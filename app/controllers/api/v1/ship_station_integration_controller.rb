c31c764aa74ce9dc91ddb1da6fc840d10a369f11
remy727 <adamovicivan123@gmail.com>
2021-01-21 13:59:21 -0500

    Fix parsing response

--- app/controllers/api/v1/ship_station_integration_controller.rb
+++ app/controllers/api/v1/ship_station_integration_controller.rb
@@ -4,9 +4,9 @@ class Api::V1::ShipStationIntegrationController < ActionController::API
   skip_before_action :verify_authenticity_token, raise: false
 
   def handle_new_order
-    data = JSON.parse(response.body)
-    resource_type = data["resource_type"]
-    resource_url = data["resource_url"]
+    puts response.body
+    resource_type = params[:resource_type]
+    resource_url = params[:resource_url]
 
     puts "app_log(INFO): resource_type = #{resource_type}"
     puts "app_log(INFO): resource_url = #{resource_url}"
