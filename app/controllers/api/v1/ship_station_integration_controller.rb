559b902a2d3a861f89bce04ac07b5354213765db
remy727 <adamovicivan123@gmail.com>
2021-01-21 18:44:29 -0500

    Remove test code

--- app/controllers/api/v1/ship_station_integration_controller.rb
+++ app/controllers/api/v1/ship_station_integration_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::ShipStationIntegrationController < ActionController::API
   skip_before_action :verify_authenticity_token, raise: false
 
   def handle_new_order
-    puts response.body
     resource_type = params[:resource_type]
     resource_url = params[:resource_url]
 
