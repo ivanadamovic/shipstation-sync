4205d39b427ddb7b04e7fa656378c3a6e132b06c
remy727 <adamovicivan123@gmail.com>
2021-01-21 13:38:18 -0500

    Fix InvalidAuthenticityToken

--- app/controllers/api/v1/ship_station_integration_controller.rb
+++ app/controllers/api/v1/ship_station_integration_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-class Api::V1::ShipStationIntegrationController < ApplicationController
+class Api::V1::ShipStationIntegrationController < ActionController::API
+  skip_before_action :verify_authenticity_token
+
   def handle_new_order
     data = JSON.parse(response.body)
     resource_type = data["resource_type"]
