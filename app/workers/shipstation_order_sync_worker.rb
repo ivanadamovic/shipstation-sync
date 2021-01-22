ce20f3fcf999b2f31646d92c4080bf1e4544f563
remy727 <adamovicivan123@gmail.com>
2021-01-22 11:37:20 -0500

    Upload logs

--- app/workers/shipstation_order_sync_worker.rb
+++ app/workers/shipstation_order_sync_worker.rb
@@ -8,23 +8,23 @@ class ShipstationOrderSyncWorker
     order = Shipstation::Order.retrieve(order_id)
     open_status = ["awaiting_payment", "awaiting_shipment", "on_hold"]
     if open_status.include? order["orderStatus"]
-      puts "app_log(INFO): checking order #{order["orderNumber"]} ..."
-      puts "app_log(INFO): order id = #{order["orderId"]}"
-      puts "app_log(INFO): order status = #{order["orderStatus"]}"
+      puts "app_log(INFO, #{order["orderNumber"]}): started checking ..."
+      puts "app_log(INFO, #{order["orderNumber"]}): order id = #{order["orderId"]}"
+      puts "app_log(INFO, #{order["orderNumber"]}): order status = #{order["orderStatus"]}"
       custom_field1 = order["advancedOptions"]["customField1"]
-      puts "app_log(INFO): order customField1 = #{custom_field1}"
+      puts "app_log(INFO, #{order["orderNumber"]}): order customField1 = #{custom_field1}"
       if custom_field1.nil?
         begin
           order_payload = MyLib::CustomShipstation.order_update_payload(order: order)
           res = Shipstation::Order.create(order_payload)
-          puts "app_log(INFO): updated order id = #{res["orderId"]}"
-          puts "app_log(INFO): updated order number = #{res["orderNumber"]}"
-          puts "app_log(INFO): updated customField1 = #{res["advancedOptions"]["customField1"]}"
+          puts "app_log(INFO, #{order["orderNumber"]}): updated order id = #{res["orderId"]}"
+          puts "app_log(INFO, #{order["orderNumber"]}): updated order number = #{res["orderNumber"]}"
+          puts "app_log(INFO, #{order["orderNumber"]}): updated customField1 = #{res["advancedOptions"]["customField1"]}"
         rescue => err
-          puts "app_log(ERROR): #{err.message}"
+          puts "app_log(ERROR, #{order["orderNumber"]}): #{err.message}"
         end
       end
-      puts "app_log(INFO): done"
+      puts "app_log(INFO, #{order["orderNumber"]}): done"
     end
   end
 end
