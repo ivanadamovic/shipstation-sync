06194ac84c3e503e484db326e61ed1606fe0a418
remy727 <adamovicivan123@gmail.com>
2021-01-22 14:27:52 -0500

    Add service code

--- app/workers/shipstation_order_sync_worker.rb
+++ app/workers/shipstation_order_sync_worker.rb
@@ -9,16 +9,17 @@ class ShipstationOrderSyncWorker
     open_status = ["awaiting_payment", "awaiting_shipment", "on_hold"]
     if open_status.include? order["orderStatus"]
       puts "app_log(INFO, #{order["orderNumber"]}): started checking ..."
-      puts "app_log(INFO, #{order["orderNumber"]}): order id = #{order["orderId"]}"
-      puts "app_log(INFO, #{order["orderNumber"]}): order status = #{order["orderStatus"]}"
+      puts "app_log(INFO, #{order["orderNumber"]}): orderId = #{order["orderId"]}"
+      puts "app_log(INFO, #{order["orderNumber"]}): orderStatus = #{order["orderStatus"]}"
+      puts "app_log(INFO, #{order["orderNumber"]}): serviceCode = #{order["serviceCode"]}"
       custom_field1 = order["advancedOptions"]["customField1"]
-      puts "app_log(INFO, #{order["orderNumber"]}): order customField1 = #{custom_field1}"
+      puts "app_log(INFO, #{order["orderNumber"]}): customField1 = #{custom_field1}"
       if custom_field1.nil?
         begin
           order_payload = MyLib::CustomShipstation.order_update_payload(order: order)
           res = Shipstation::Order.create(order_payload)
-          puts "app_log(INFO, #{order["orderNumber"]}): updated order id = #{res["orderId"]}"
-          puts "app_log(INFO, #{order["orderNumber"]}): updated order number = #{res["orderNumber"]}"
+          puts "app_log(INFO, #{order["orderNumber"]}): updated orderId = #{res["orderId"]}"
+          puts "app_log(INFO, #{order["orderNumber"]}): updated orderNumber = #{res["orderNumber"]}"
           puts "app_log(INFO, #{order["orderNumber"]}): updated customField1 = #{res["advancedOptions"]["customField1"]}"
         rescue => err
           puts "app_log(ERROR, #{order["orderNumber"]}): #{err.message}"
