6bca7d2b89476141e884ae116db6b1196ced09d9
remy727 <adamovicivan123@gmail.com>
2021-02-04 10:49:18 -0500

    Refactor: fix routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
-  root :to => 'home#index'
-  get '/products', :to => 'products#index'
-  mount ShopifyApp::Engine, at: '/'
+  root to: "home#index"
+  get "/products", to: "products#index"
+  mount ShopifyApp::Engine, at: "/"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
-  	namespace :v1 do
-  		post "new_order" => "ship_station_integration#handle_new_order"
-  	end
+    namespace :v1 do
+      post "new_order" => "ship_station_integration#handle_new_order"
+    end
   end
 end
