417ecefbb8ae4f81166f31fc1dd0fd95b25305f7
remy727 <adamovicivan123@gmail.com>
2021-02-17 04:17:11 -0500

    Fix: add detailed shipping logs

--- app/workers/shipstation_shipped_order_sync_worker.rb
+++ app/workers/shipstation_shipped_order_sync_worker.rb
@@ -6,7 +6,7 @@ class ShipstationShippedOrderSyncWorker
 
   def perform(order_id)
     order = Shipstation::Order.retrieve(order_id)
-    puts "app_ship_log(INFO, #{order["orderNumber"]}): started checking ..."
+    puts "app_ship_log(INFO, #{order["orderNumber"]}): started checking order(#{order_id}, #{order["orderNumber"]})..."
     puts "app_ship_log(INFO, #{order["orderNumber"]}): orderStatus = #{order["orderStatus"]}"
     if order["orderStatus"] == "shipped"
       # Insert shipstation order
@@ -19,12 +19,16 @@ class ShipstationShippedOrderSyncWorker
       )
       # Create items
       order["items"].each { |item|
-        shipstation_order.items.create(
-          sku: item["sku"],
-          name: item["name"],
-          options: item["options"],
-          quantity: item["quantity"]
-        )
+        begin
+          shipstation_order.items.create!(
+            sku: item["sku"],
+            name: item["name"],
+            options: item["options"],
+            quantity: item["quantity"]
+          )
+        rescue => e
+          puts "app_ship_log(ERROR, #{order["orderNumber"]}): #{e.message}"
+        end
       }
       puts "app_ship_log(INFO, #{order["orderNumber"]}): done"
     end
