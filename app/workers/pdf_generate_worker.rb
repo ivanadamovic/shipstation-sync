da8ad9d1242f046f1bc053f1891de795ec5f0624
remy727 <adamovicivan123@gmail.com>
2021-02-17 13:23:11 -0500

    Fix: adjust PDF size

--- app/workers/pdf_generate_worker.rb
+++ app/workers/pdf_generate_worker.rb
@@ -5,12 +5,19 @@ class PdfGenerateWorker
   sidekiq_options queue: :pdf_generate, retry: false
 
   def perform(*args)
-    pdf = WickedPdf.new.pdf_from_url("http://localhost:7000/orders/1")
-    # then save to a file
-    save_path = Rails.root.join("tmp", "235400.pdf")
-    File.delete(save_path)
-    File.open(save_path, "wb") do |file|
-      file << pdf
+    begin
+      pdf = WickedPdf.new.pdf_from_url("http://localhost:7000/orders/1", {
+        page_width: "8.5in",
+        page_height: "11in"
+      })
+      # then save to a file
+      save_path = Rails.root.join("tmp", "235400.pdf")
+      File.delete(save_path)
+      File.open(save_path, "wb") do |file|
+        file << pdf
+      end
+    rescue => e
+      puts "app_log(INFO): #{e.message}"
     end
   end
 end
